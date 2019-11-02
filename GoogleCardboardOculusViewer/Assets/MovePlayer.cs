e1e06f874ebc453919a638823e832599a772be8e
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-11-02 11:35:48 -0500

    init

---
+++ GoogleCardboardOculusViewer/Assets/MovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MovePlayer : MonoBehaviour
+{
+    public int speed = 0;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // get input data from keyboard or controller
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // update player position based on input
+        Vector3 position = transform.position;
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        transform.position = position;
+    }
+}
---
+++ vrjpeg_example/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VrJpeg;
+using MetadataExtractor.Formats.Xmp;
+
+namespace vrjpeg_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"Resources\IMG_20170723_142255.vr.jpg";
+            string filenameWithoutExtension = filename.Substring(0, filename.Length - 7);
+            Debug.WriteLine("{0}", filenameWithoutExtension);
+    
+            // TODO: Save to sub folder
+            // Read raw XMP metadata.
+            var xmpDirectories = VrJpegMetadataReader.ReadMetadata(filename);
+
+            // Parse metadata into a dedicated class.
+            GPanorama pano = new GPanorama(xmpDirectories.ToList());
+
+            // Extract embedded image.
+            // The primary image is the left eye, the right eye is embedded in the metadata.
+            if (pano.ImageData != null)
+            {
+                string rightEyeFilename = string.Format("{0}_right.jpg", filenameWithoutExtension);
+                string rightEyeFile = Path.Combine(Path.GetDirectoryName(filename), filenameWithoutExtension, rightEyeFilename);
+
+                File.WriteAllBytes(rightEyeFile, pano.ImageData);
+            }
+
+            // Extract embedded audio.
+            if (pano.AudioData != null)
+            {
+                string audioFilename = string.Format("{0}_audio.mp4", filenameWithoutExtension);
+                string audioFile = Path.Combine(Path.GetDirectoryName(filename), filenameWithoutExtension, audioFilename);
+
+                File.WriteAllBytes(audioFile, pano.AudioData);
+            }
+
+        }
+
+        private static void DumpProperties(IEnumerable<XmpDirectory> directories)
+        {
+            foreach (var dir in directories)
+            {
+                foreach (var prop in dir.XmpMeta.Properties)
+                {
+                if (prop.Path != null && prop.Path.EndsWith(":Data"))
+                    Debug.WriteLine("{0} - {1}", prop.Namespace, prop.Path);
+                else
+                    Debug.WriteLine("{0} - {1}:{2}", prop.Namespace, prop.Path, prop.Value);
+                }
+            }
+        }
+    }
+}

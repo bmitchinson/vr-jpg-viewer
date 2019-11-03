ca61130d387452f0dbcc9d6f4639270bd7c8578f
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-11-03 07:49:29 -0600

    demo!

--- GoogleCardboardOculusViewer/Assets/ButtonAction.cs
+++ GoogleCardboardOculusViewer/Assets/ButtonAction.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using VrJpeg;
+using XmpCore;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Jpeg;
+using MetadataExtractor.Formats.Xmp;
+using MetadataExtractor.IO;
+using ICSharpCode;
 
 public class ButtonAction : MonoBehaviour
 {
     public GameObject objDeleteRef;
     public Material leftMatRef;
     public Material rightMatRef;
+    public AudioSource m_MyAudioSource;
 
     public void DestroyObj()
     {
@@ -18,11 +26,15 @@ public class ButtonAction : MonoBehaviour
     public void DownloadImage()
     {
         Debug.Log("Attempting new download");
-        string textureURL = "https://firebasestorage.googleapis.com/v0/b/cardboardcameraoculusviewer.appspot.com/o/dock.vr.jpg?alt=media";
-        StartCoroutine(DownloadImage(textureURL));
+        string leftURL = "https://firebasestorage.googleapis.com/v0/b/cardboardcameraoculusviewer.appspot.com/o/upload.vr.jpg?alt=media";
+        string rightURL = "https://firebasestorage.googleapis.com/v0/b/cardboardcameraoculusviewer.appspot.com/o/core_right.jpg?alt=media";
+        string audioURL = "https://firebasestorage.googleapis.com/v0/b/cardboardcameraoculusviewer.appspot.com/o/core_audio.mp3?alt=media";
+        StartCoroutine(DownloadRightImage(rightURL));
+        StartCoroutine(DownloadLeftImage(leftURL));
+        StartCoroutine(DownloadAudio(audioURL));
     }
 
-    IEnumerator DownloadImage(string URL)
+    IEnumerator DownloadRightImage(string URL)
     {
         using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(URL))
         {
@@ -37,9 +49,47 @@ public class ButtonAction : MonoBehaviour
                 Debug.Log("Got texture");
                 Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
 
-                Debug.Log("Setting");
-                leftMatRef.mainTexture = texture;
+                Debug.Log("Setting Right");
                 rightMatRef.mainTexture = texture;
+            }
+        }
+    }
+    IEnumerator DownloadLeftImage(string URL)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(URL))
+        {
+            yield return uwr.SendWebRequest();
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                Debug.Log("Got texture");
+                Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
+
+                Debug.Log("Setting Left");
+                leftMatRef.mainTexture = texture;
+            }
+        }
+    }
+
+    IEnumerator DownloadAudio(string url)
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
+        {
+            yield return www.Send();
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Setting audio");
+                m_MyAudioSource.clip = DownloadHandlerAudioClip.GetContent(www);
+                m_MyAudioSource.loop = true;
+                m_MyAudioSource.Play();
             }
         }
     }

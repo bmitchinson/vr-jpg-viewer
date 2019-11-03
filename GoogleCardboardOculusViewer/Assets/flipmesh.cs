2b9ec472b2b8ca917ca5e8d8faec241fcf8b59d0
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-11-02 21:21:44 -0500

    mono green ring removal

---
+++ GoogleCardboardOculusViewer/Assets/flipmesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class flipmesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
